774c951689d77050e64a5c283b4748c82157f079
最萌小汐 <sumneko@hotmail.com>
2019-09-26 20:26:07 +0800

    更新测试

--- test.lua
+++ test.lua
@@ -1,3 +1,6 @@
+local thread = require 'bee.thread'
+thread.newchannel 'TEST'
+local ch = thread.channel 'TEST'
 local util = require 'utility'
 local type = type
 local laod = load
@@ -6,9 +9,18 @@ local t = {}
 for x = 1, 10000 do
     t[x] = t
 end
+
 local clock = os.clock()
 for _ = 1, 100 do
-    local new = util.unpack(t)
-    local old = util.pack(new)
+    local dump = util.unpack(t)
+    util.pack(dump)
+end
+print(os.clock() - clock)
+
+local dump = util.unpack(t)
+local clock = os.clock()
+for _ = 1, 100 do
+    ch:push(dump)
+    ch:pop()
 end
 print(os.clock() - clock)
