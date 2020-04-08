256e2b3d60f177c1b3f73bc02b8d69e618fb14e8
最萌小汐 <sumneko@hotmail.com>
2020-04-08 17:49:49 +0800

    更新

--- recursive.lua
+++ recursive.lua
@@ -1,16 +1,24 @@
 local wrap        = coroutine.wrap
+local cocreate    = coroutine.create
+local status      = coroutine.status
 local yield       = coroutine.yield
 local pack        = table.pack
 local unpack      = table.unpack
 local isyieldable = coroutine.isyieldable
 
 local Stacks = {}
+local YieldMark = {}
+
+local First = true
+local Finish = setmetatable({}, { __close = function ()
+    First = true
+end })
 
 local function createStack(func, ...)
     local s = {
         thd = wrap(func),
         arg = pack(...),
-        res = nil,
+        st  = 0,
     }
     Stacks[#Stacks+1] = s
     return s
@@ -18,50 +26,66 @@ end
 
 local function getResult(stack)
     local res = stack.res
+    if not res then
+        return
+    end
     return unpack(res, 1, res.n)
 end
 
-local function getArgs(stack)
+local function getArg(stack)
     local arg = stack.arg
+    if not arg then
+        return
+    end
     return unpack(arg, 1, arg.n)
 end
 
-local function callStack(stack)
+local function callStack(stack, ...)
     local thd = stack.thd
-    stack.res = pack(thd(getArgs(stack)))
+    local res = pack(thd(...))
+    if res[1] == YieldMark then
+        stack.st = 1
+    else
+        stack.st = 2
+        stack.res = res
+    end
 end
 
 local function firstCall(max, func, ...)
-    local stack = createStack(func, ...)
+    local lastStack = createStack(func, ...)
     for _ = 1, max do
         local len = #Stacks
-        if len <= 0 then
-            return getResult(stack)
+        local stack = Stacks[len]
+        if stack.st == 0 then
+            callStack(stack, getArg(stack))
+        elseif stack.st == 1 then
+            callStack(stack, getResult(lastStack))
+        else
+            if len == 1 then
+                return getResult(stack)
+            end
+            lastStack = stack
+            Stacks[len] = nil
         end
-        stack = Stacks[len]
-        Stacks[len] = nil
-        callStack(stack)
     end
     error('stack overflow!')
 end
 
 local function subCall(func, ...)
     createStack(func, ...)
-    yield()
+    return yield(YieldMark)
 end
 
 local m = {}
-local mt = {}
-mt.__index = mt
 
-m.max = 100000
-
-function m.resolve(func)
+function m.resolve(func, max)
     return function (...)
-        if isyieldable() then
-            return subCall(func, ...)
+        if First then
+            First = false
+            local finish <close> = Finish
+            return firstCall(max or 10000, func, ...)
         else
-            return firstCall(m.max, func, ...)
+            return subCall(func, ...)
         end
     end
 end
--- test/trecursive.lua
+++ test/trecursive.lua
@@ -1,17 +1,20 @@
 local rec = require 'recursive'
 
+local t = {}
 local function f(i)
-    if i >= 10000 then
+    if i <= 1 then
         return i
     else
         -- 避免尾调用不入栈
-        local n = f(i+1)
-        return n
+        local n = f(i-1)
+        t[#t+1] = n
+        return i * 2
     end
 end
 
-f = rec.resolve(f)
+f = rec.resolve(f, 100000)
 
 local clock = os.clock()
-print(f(0))
+f(10000)
 print(os.clock() - clock)
+assert(#t == 9999)
