3a6ff62f40ea4b92dcbe69d5b7a81257688a23a2
最萌小汐 <sumneko@hotmail.com>
2020-04-08 14:11:18 +0800

    整理代码

--- recursive.lua
+++ recursive.lua
@@ -1,61 +1,67 @@
+local wrap        = coroutine.wrap
+local yield       = coroutine.yield
+local pack        = table.pack
+local unpack      = table.unpack
+local isyieldable = coroutine.isyieldable
+
+local Stacks = {}
+
+local function createStack(func, ...)
+    local s = {
+        thd = wrap(func),
+        arg = pack(...),
+        res = nil,
+    }
+    Stacks[#Stacks+1] = s
+    return s
+end
+
+local function getResult(stack)
+    local res = stack.res
+    return unpack(res, 1, res.n)
+end
+
+local function getArgs(stack)
+    local arg = stack.arg
+    return unpack(arg, 1, arg.n)
+end
+
+local function callStack(stack)
+    local thd = stack.thd
+    stack.res = pack(thd(getArgs(stack)))
+end
+
+local function firstCall(max, func, ...)
+    local stack = createStack(func, ...)
+    for _ = 1, max do
+        local len = #Stacks
+        if len <= 0 then
+            return getResult(stack)
+        end
+        stack = Stacks[len]
+        Stacks[len] = nil
+        callStack(stack)
+    end
+    error('stack overflow!')
+end
+
+local function subCall(func, ...)
+    createStack(func, ...)
+    yield()
+end
+
 local m = {}
 local mt = {}
 mt.__index = mt
 
 m.max = 100000
-m._stacks = {}
-m._mark = {}
-
-function m._createStack(func, ...)
-    local s = {
-        thd = coroutine.wrap(func),
-        arg = table.pack(...),
-        res = nil,
-    }
-    m._stacks[#m._stacks+1] = s
-    return s
-end
-
-function m._getResult(stack)
-    local res = stack.res
-    return table.unpack(res, 1, res.n)
-end
-
-function m._getArgs(stack)
-    local arg = stack.arg
-    return table.unpack(arg, 1, arg.n)
-end
-
-function m._callStack(stack)
-    local thd = stack.thd
-    stack.res = table.pack(thd(m._getArgs(stack)))
-end
-
-function m._firstCall(func, ...)
-    local stack = m._createStack(func, ...)
-    for _ = 1, m.max do
-        local len = #m._stacks
-        if len <= 0 then
-            return m._getResult(stack)
-        end
-        stack = m._stacks[len]
-        m._stacks[len] = nil
-        m._callStack(stack)
-    end
-    error('stack overflow!')
-end
-
-function m._subCall(func, ...)
-    m._createStack(func, ...)
-    coroutine.yield()
-end
 
 function m.resolve(func)
     return function (...)
-        if coroutine.isyieldable() then
-            return m._subCall(func, ...)
+        if isyieldable() then
+            return subCall(func, ...)
         else
-            return m._firstCall(func, ...)
+            return firstCall(m.max, func, ...)
         end
     end
 end
