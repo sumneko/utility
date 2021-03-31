5b9d1a712e93ec6c727999825f6cb477ff4a7b48
Mr.Zhuang <w4454962@users.noreply.github.com>
2021-03-31 13:16:10 +0800

    修复hex:encode的bug
    修正encode  使decode -> encode ->decode 的内容保证一致。

--- hex.lua
+++ hex.lua
@@ -3,7 +3,7 @@ local m = {}
 local mt = {}
 mt.__index = mt
 
-local function splitDefine(def)
+local function splitDefine(def, i)
     local k, fmt = def:match '^(.-)%:(.+)$'
     local index
     local a, b = fmt:find '%[.-%]'
@@ -11,75 +11,69 @@ local function splitDefine(def)
         index = fmt:sub(a + 1, b - 1)
         fmt = fmt:sub(1, a - 1)
     end
+	if k == nil or k:len() == 0 or k == "" then
+		k = i
+	end
     return k, fmt, index
 end
 
 function mt:decode(hex)
     local define = self._define
     local idx = 1
-    local link = {}
     local root = {}
     local buildExp, buildChunk, buildCase
 
-    local function newLink(p)
-        local t = {}
-        link[t] = p
-        return t
-    end
+	local map = {}
 
-    local function getIndex(t, k)
-        if not t then
-            return nil
-        end
-        if t[k] ~= nil then
-            return t[k]
-        end
-        return getIndex(link[t], k)
-    end
-
-    buildExp = function (ct, exp)
-        local k, fmt, index = splitDefine(exp)
+    buildExp = function (ct, exp, i, stack)
+        local k, fmt, index = splitDefine(exp, i)
         local fmtDef = define[fmt]
         if fmtDef then
             if index then
                 assert(ct[index], 'No index:' .. index)
                 ct[k] = {}
                 for x = 1, ct[index] do
-                    ct[k][x] = newLink(ct)
-                    buildChunk(ct[k][x], fmtDef)
+                    ct[k][x] = {}
+                    buildChunk(ct[k][x], fmtDef, stack)
                 end
             else
-                ct[k] = newLink(ct)
-                buildChunk(ct[k], fmtDef)
+                ct[k] = {}
+                buildChunk(ct[k], fmtDef, stack)
             end
         else
             ct[k], idx = fmt:unpack(hex, idx)
         end
     end
 
-    buildCase = function (ct, case)
+    buildCase = function (ct, case, i, stack)
         local env = setmetatable({}, { __index = function (_, k)
-            return getIndex(ct, k)
+            for a = stack, 0, -1 do
+				local v = map[a][k]
+				if v then
+					return v
+				end
+			end
         end })
         local caseBuf = 'return ' .. case.case
         local caseF = assert(load(caseBuf, caseBuf, 't', env))
         if caseF() then
-            buildExp(ct, case.exp)
+            buildExp(ct, case.exp, i, stack)
         end
     end
 
-    buildChunk = function (ct, cdef)
+    buildChunk = function (ct, cdef, stack)
+		map[stack] = ct
         for i = 1, #cdef do
             if type(cdef[i]) == 'string' then
-                buildExp(ct, cdef[i])
+                buildExp(ct, cdef[i], i, stack + 1)
             elseif cdef[i].type == 'case' then
-                buildCase(ct, cdef[i])
+                buildCase(ct, cdef[i], i, stack + 1)
             end
         end
         ct[''] = nil
     end
 
-    buildChunk(root, define)
+    buildChunk(root, define, 0)
 
     return root
 end
@@ -89,17 +83,19 @@ function mt:encode(data)
     local buf = {}
     local buildChunk, buildExp
 
-    buildExp = function (ct, exp)
-        local k, fmt, index = splitDefine(exp)
+	local map = {}
+
+    buildExp = function (ct, exp, i, stack)
+        local k, fmt, index = splitDefine(exp, i)
         local fmtDef = define[fmt]
         if fmtDef then
             if index then
                 assert(ct[index], 'No index:' .. index)
                 for x = 1, ct[index] do
-                    buildChunk(ct[k][x], fmtDef)
+                    buildChunk(ct[k][x], fmtDef, stack)
                 end
             else
-                buildChunk(ct[k], fmtDef)
+                buildChunk(ct[k], fmtDef, stack)
             end
         else
             local v = ct[k]
@@ -116,18 +112,35 @@ function mt:encode(data)
         end
     end
 
-    buildChunk = function (ct, cdef)
+	buildCase = function (ct, case, i, stack)
+        local env = setmetatable({}, { __index = function (_, k)
+			for a = stack, 0, -1 do
+				local v = map[a][k]
+				if v then
+					return v
+				end
+			end
+        end })
+        local caseBuf = 'return ' .. case.case
+        local caseF = assert(load(caseBuf, caseBuf, 't', env))
+        if caseF() then
+            buildExp(ct, case.exp, i, stack)
+        end
+    end
+
+    buildChunk = function (ct, cdef, stack)
+		map[stack] = ct
         for i = 1, #cdef do
             if type(cdef[i]) == 'string' then
-                buildExp(ct, cdef[i])
+                buildExp(ct, cdef[i], i, stack + 1)
             elseif cdef[i].type == 'case' then
                 -- TODO
-                --buildCase(ct, cdef[i])
+                buildCase(ct, cdef[i], i, stack + 1)
             end
         end
     end
 
-    buildChunk(data, define)
+    buildChunk(data, define, 0)
 
     return table.concat(buf)
 end
