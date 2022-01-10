c784ad0730778de1fc6fc5e7dde768240f703e74
最萌小汐 <sumneko@hotmail.com>
2022-01-10 17:45:11 +0800

    linked-table

--- test.lua
+++ test.lua
@@ -4,6 +4,9 @@ dofile 'test/trecursive.lua'
 dofile 'test/thex.lua'
 dofile 'test/smerger.lua'
 dofile 'test/rtable.lua'
+dofile 'test/linked.lua'
+
+-- 这个太慢了，最后测试
 dofile 'test/thex2.lua'
 
 print('测试全部完成')
---
+++ test/linked.lua
@@ -0,0 +1,36 @@
+local linkedTable = require 'linked-table'
+
+local lt = linkedTable()
+
+lt:pushTail(1)
+lt:pushTail(2)
+lt:pushTail(3)
+lt:pushHead(4)
+assert(lt:dump() == '4 1 2 3')
+
+lt:pop(2)
+assert(lt:dump() == '4 1 3')
+
+lt:popHead()
+assert(lt:dump() == '1 3')
+
+lt:pushHead(2)
+lt:pushHead(4)
+lt:pushHead(6)
+lt:pushHead(8)
+assert(lt:dump() == '8 6 4 2 1 3')
+
+lt:popTail()
+assert(lt:dump() == '8 6 4 2 1')
+
+lt:pushAfter(5, 4)
+assert(lt:dump() == '8 6 4 5 2 1')
+
+lt:pushBefore(7, 4)
+assert(lt:dump() == '8 6 7 4 5 2 1')
+
+lt:replace(4, 3)
+assert(lt:dump() == '8 6 7 3 5 2 1')
+
+lt:reset()
+assert(lt:dump() == '')
