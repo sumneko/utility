0c64bc10662bcff76f3ae266a6fc862ef206ffde
最萌小汐 <sumneko@hotmail.com>
2022-09-02 16:10:56 +0800

    update doctor

--- doctor.lua
+++ doctor.lua
@@ -162,6 +162,9 @@ local function private(o)
 end
 
 local m = private {}
+
+m._ignoreMainThread = true
+
 --- 获取内存快照，生成一个内部数据结构。
 --- 一般不用这个API，改用 report 或 catch。
 ---@return table
@@ -176,7 +179,9 @@ m.snapshot = private(function ()
             exclude[o] = true
         end
     end
-    ---@return any
+    ---@generic T
+    ---@param o T
+    ---@return T
     local function private(o)
         if not o then
             return nil
@@ -189,7 +194,6 @@ m.snapshot = private(function ()
 
     local find
     local mark = private {}
-
 
     local function findTable(t, result)
         result = result or {}
@@ -210,21 +214,45 @@ m.snapshot = private(function ()
             if not wk then
                 local keyInfo = find(k)
                 if keyInfo then
-                    result[#result+1] = private {
-                        type = 'key',
-                        name = formatName(k),
-                        info = keyInfo,
-                    }
+                    if wv then
+                        find(v)
+                        local valueResults = mark[v]
+                        if valueResults then
+                            valueResults[#valueResults+1] = private {
+                                type = 'weakvalue-key',
+                                name = formatName(t) .. '|' .. formatName(v),
+                                info = keyInfo,
+                            }
+                        end
+                    else
+                        result[#result+1] = private {
+                            type = 'key',
+                            name = formatName(k),
+                            info = keyInfo,
+                        }
+                    end
                 end
             end
             if not wv then
                 local valueInfo = find(v)
                 if valueInfo then
-                    result[#result+1] = private {
-                        type = 'field',
-                        name = formatName(k) .. '|' .. formatName(v),
-                        info = valueInfo,
-                    }
+                    if wk then
+                        find(k)
+                        local keyResults = mark[k]
+                        if keyResults then
+                            keyResults[#keyResults+1] = private {
+                                type = 'weakkey-field',
+                                name = formatName(t) .. '|' .. formatName(k),
+                                info = valueInfo,
+                            }
+                        end
+                    else
+                        result[#result+1] = private {
+                            type = 'field',
+                            name = formatName(k) .. '|' .. formatName(v),
+                            info = valueInfo,
+                        }
+                    end
                 end
             end
         end
@@ -551,7 +579,7 @@ m.exclude = private(function (...)
 end)
 
 --- 比较2个报告
----@return string
+---@return table
 m.compare = private(function (old, new)
     local newHash = {}
     local ret = {}
