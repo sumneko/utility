d2ac5315dde2eb896985b840a1cc696508693a3f
最萌小汐 <sumneko@hotmail.com>
2026-07-07 19:23:09 +0800

    简单的表格验证器

---
+++ tamper-checker.lua
@@ -0,0 +1,182 @@
+---@class TamperChecker
+local M = {}
+M.__index = M
+
+---@class TamperChecker.Status
+---@field hash integer # 表的哈希值，用于校验
+---@field name string # 表的名称，子表用 `.` 来拼接
+
+local clock = os.clock
+
+local weakK = { __mode = "k" }
+
+---@param str string
+---@return integer
+local function hash(str)
+    local h = 0xcbf29ce484222325
+    local bytes = { string.byte(str, 1, -1) }
+    for i = 1, #bytes do
+        h = (h ~ bytes[i]) * 0x100000001b3
+    end
+    return h
+end
+
+---@param v any
+---@return string
+local function valueHash(v)
+    local tp = type(v)
+    if tp == 'string' then
+        return 'string:' .. hash(v)
+    elseif tp == 'boolean'
+    or tp == 'number'
+    or tp == 'nil' then
+        return ('%q'):format(v)
+    else
+        return ('%s:%p'):format(tp, v)
+    end
+end
+
+---@param t table
+---@return integer
+local function makeHash(t)
+    local buf = {}
+    for k, v in pairs(t) do
+        buf[#buf + 1] = valueHash(k) .. '=' .. valueHash(v)
+    end
+    table.sort(buf)
+    local concated = table.concat(buf, ",")
+    return hash(concated)
+end
+
+---@package
+---@param isWeakRef? boolean
+function M:init(isWeakRef)
+    ---@private
+    ---@type table<table, TamperChecker.Status>
+    self.tables = {}
+    ---@private
+    self.clock = clock
+    if isWeakRef then
+        setmetatable(self.tables, weakK)
+    end
+end
+
+---将一张表加入检查
+---@param t table
+---@param name string # 表的名称
+---@return boolean
+function M:add(t, name)
+    if self.tables[t] then
+        return false
+    end
+    local status = {
+        hash = makeHash(t),
+        name = name,
+    }
+    self.tables[t] = status
+
+    for k, v in pairs(t) do
+        if type(v) == 'table' then
+            self:add(v, name .. '.' .. tostring(k))
+        end
+    end
+
+    return true
+end
+
+---@private
+---@type table?
+M.lastTable = nil
+
+---检查所有加入的表是否被篡改
+---@param maxTime? number # 最大检查时间（秒）
+---@return string[]?
+---@return 'finished' | 'timeout'
+function M:checkAll(maxTime)
+    local startTime = self.clock()
+
+    local currentTable = self.lastTable
+    local tampered = nil
+    while true do
+        if maxTime and maxTime + startTime < self.clock() then
+            self.lastTable = currentTable
+            return tampered, 'timeout'
+        end
+        local status = self.tables[currentTable]
+        if currentTable and status and makeHash(currentTable) ~= status.hash then
+            tampered = tampered or {}
+            tampered[#tampered+1] = status.name
+        end
+        currentTable = next(self.tables, currentTable)
+        if currentTable == self.lastTable then
+            break
+        end
+    end
+
+    self.lastTable = currentTable
+    return tampered, 'finished'
+end
+
+---立即检查指定表是否被篡改
+---@param t table
+---@param recursive? boolean # 是否递归检查子表
+function M:check(t, recursive)
+    local status = self.tables[t]
+    if not status then
+        return false
+    end
+
+    if makeHash(t) ~= status.hash then
+        return true
+    end
+
+    if recursive then
+        local visited = {}
+
+        local function lookInto(c)
+            visited[c] = true
+
+            for k, v in pairs(c) do
+                if type(v) == 'table' and not visited[v] then
+                    local cstatus = self.tables[v]
+                    if not cstatus then
+                        return true
+                    end
+                    if makeHash(v) ~= cstatus.hash then
+                        return true
+                    end
+                    if lookInto(v) then
+                        return true
+                    end
+                end
+            end
+
+            return false
+        end
+
+        return lookInto(t)
+    end
+
+    return false
+end
+
+---@param clockFunc fun():number # 返回当前时间（秒）的函数
+function M:setClockFunc(clockFunc)
+    self.clock = clockFunc
+end
+
+local API = {}
+
+---@param isWeakRef? boolean # 是否是弱引用，如果是会使用弱表
+function API.create(isWeakRef)
+    local checker = setmetatable({}, M)
+    checker:init(isWeakRef)
+    return checker
+end
+
+---@param clockFunc fun():number # 返回当前时间（秒）的函数
+function API.setClockFunc(clockFunc)
+    clock = clockFunc
+end
+
+return API
---
+++ test/tamper-checker.lua
@@ -0,0 +1,107 @@
+local tc = require 'tamper-checker'
+
+do
+    local instance = tc.create()
+
+    local t = { x = 1, y = 2 }
+
+    instance:add(t, 'test')
+
+    assert(instance:check(t) == false)
+    assert(instance:checkAll() == nil)
+
+    t.x = 3
+
+    assert(instance:check(t) == true)
+    local tampered = instance:checkAll()
+    assert(tampered ~= nil)
+    assert(#tampered == 1)
+    assert(tampered[1] == 'test')
+end
+
+do
+    local instance = tc.create()
+
+    local t = { x = 1, y = 2, z = { a = 1, b = 2 } }
+
+    instance:add(t, 'test')
+
+    assert(instance:check(t) == false)
+    assert(instance:checkAll() == nil)
+
+    t.z.a = 3
+
+    assert(instance:check(t) == false)
+    assert(instance:check(t.z) == true)
+    assert(instance:check(t, true) == true)
+    local tampered = instance:checkAll()
+    assert(tampered ~= nil)
+    assert(#tampered == 1)
+    assert(tampered[1] == 'test.z')
+end
+
+do
+    local time = 0
+    local instance = tc.create()
+    instance:setClockFunc(function ()
+        time = time + 1
+        return time
+    end)
+
+    local large = {}
+
+    for i = 1, 10000 do
+        large[i] = {}
+        for j = 1, 5 do
+            large[i][j] = j
+        end
+        for j = 6, 10 do
+            large[i][j] = tostring(j)
+        end
+    end
+
+
+    instance:add(large, 'test')
+
+    large[9000].x = -1
+
+    do
+        local tampered, status = instance:checkAll()
+        assert(status == 'finished')
+        assert(tampered ~= nil)
+        assert(#tampered == 1)
+        assert(tampered[1] == 'test.9000')
+    end
+
+    do
+        time = 0
+
+        local tampered1, status1 = instance:checkAll(4000)
+        assert(time == 4002)
+        assert(status1 == 'timeout')
+
+        local tampered2, status2 = instance:checkAll(4000)
+        assert(time == 8004)
+        assert(status2 == 'timeout')
+
+        local tampered3, status3 = instance:checkAll(4000)
+        assert(time == 12006)
+        assert(status3 == 'timeout')
+
+        local tampered4 = tampered1 or tampered2 or tampered3
+        assert(tampered4 ~= nil)
+        assert(#tampered4 == 1)
+        assert(tampered4[1] == 'test.9000')
+
+        local clock1 = os.clock()
+        local tampered5, status5 = instance:checkAll()
+        local clock2 = os.clock()
+        assert(time == 12007)
+        assert(status5 == 'finished')
+        assert(tampered5 ~= nil)
+        assert(#tampered5 == 1)
+        assert(tampered5[1] == 'test.9000')
+
+        print('tamper-checker 平均耗时', (clock2 - clock1) / #large * 1000 * 1000, '微秒')
+    end
+end
